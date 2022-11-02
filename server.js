a9a9274c0f2be31baa318a98cff10e7d46ef75eb
pawgog <pgogola10@gmail.com>
2022-11-02 17:54:48 +0100

    Add router to express server

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ require('dotenv').config();
 
 mongoose.connect(`${process.env.DB_URL}/products`);
 const db = mongoose.connection;
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connect to DB'))
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connect to DB'));
+
+app.use(express.json());
+
+const productsRouter = require('./routes/products');
+app.use('products', productsRouter);
 
 app.listen(3000, () => console.log('server is starting'));
