526085711c422f4a6ee53fa16c2c0950f28c9fc7
pawgog <pgogola10@gmail.com>
2022-11-02 18:38:59 +0100

    Set up get products server request

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const port = process.env.PORT || 3000;
 
 require('dotenv').config();
 
@@ -12,6 +13,8 @@ db.once('open', () => console.log('Connect to DB'));
 app.use(express.json());
 
 const productsRouter = require('./routes/products');
-app.use('products', productsRouter);
+app.use('/products', productsRouter);
 
-app.listen(3000, () => console.log('server is starting'));
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}!`)
+})
