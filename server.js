ccf17e383d6707a8a1b14689f5b94a128631fe3a
pawgog <pgogola10@gmail.com>
2022-11-02 17:48:46 +0100

    Settings to connect to local DB

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+mongoose.connect(`${process.env.DB_URL}/products`);
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connect to DB'))
 
 app.listen(3000, () => console.log('server is starting'));
