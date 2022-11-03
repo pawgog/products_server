0213771a7128a908d6039cbc6033defeca5806ff
pawgog <pgogola10@gmail.com>
2022-11-03 17:59:40 +0100

    Add products model and get request

---
+++ models/products.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const productsSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: String,
+        required: true
+    },
+    imgUrl: {
+        type: String,
+        required: true
+    },
+});
+
+module.exports = mongoose.model('Products', productsSchema);
--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const Products = require('../models/products');
 
-router.get('/', (req, res) => {
-    res.send('get products');
+router.get('/', async (req, res) => {
+    try {
+        const products = await Products.find();
+        res.send(products);
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 })
 
 module.exports = router;
