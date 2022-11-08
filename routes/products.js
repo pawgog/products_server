5eab8e57662c82b072c29bf6f851da686e5265c9
pawgog <pgogola10@gmail.com>
2022-11-08 18:21:39 +0100

    Add post request

--- routes/products.js
+++ routes/products.js
@@ -11,4 +11,31 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/:id', async (req, res) => {
+    res.send(req.params.id)
+})
+
+router.post('/:id', async (req, res) => {
+    const product = new Products({
+        name: req.body.name,
+        price: req.body.price,
+        imgUrl: req.body.imgUrl
+    })
+
+    try {
+        const newProduct = await product.save()
+        res.status(201).json(newProduct)
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    res.send(req.params.id)
+})
+
+router.delete('/:id', async (req, res) => {
+    res.send(req.params.id)
+})
+
 module.exports = router;
