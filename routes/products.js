998319a200b58aaab39daa6b52de1ca4f6d26e26
pawgog <pgogola10@gmail.com>
2022-11-23 18:28:01 +0100

    Finish REST api with patch request

--- routes/products.js
+++ routes/products.js
@@ -7,12 +7,12 @@ router.get('/', async (req, res) => {
         const products = await Products.find();
         res.send(products);
     } catch (error) {
-        res.status(500).json({ message: error.message })
+        res.status(500).json({ message: error.message });
     }
 })
 
 router.get('/:id', getProducts, (req, res) => {
-    res.send(res.product)
+    res.send(res.product);
 })
 
 router.post('/:id', async (req, res) => {
@@ -20,26 +20,40 @@ router.post('/:id', async (req, res) => {
         name: req.body.name,
         price: req.body.price,
         imgUrl: req.body.imgUrl
-    })
+    });
 
     try {
-        const newProduct = await product.save()
-        res.status(201).json(newProduct)
+        const newProduct = await product.save();
+        res.status(201).json(newProduct);
     } catch (error) {
-        res.status(400).json({ message: error.message })
+        res.status(400).json({ message: error.message });
     }
 })
 
-router.patch('/:id', getProducts, (req, res) => {
-
+router.patch('/:id', getProducts, async (req, res) => {
+    if(req.body.name !== null) {
+        res.product.name = req.body.name;
+    }
+    if(req.body.price !== null) {
+        res.product.price = req.body.price;
+    }
+    if(req.body.imgUrl !== null) {
+        res.product.imgUrl = req.body.imgUrl;
+    }
+    try {
+        const updateProduct = await res.product.save();
+        res.json(updateProduct)
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 })
 
 router.delete('/:id', getProducts, async (req, res) => {
     try {
-        await res.product.remove()
-        res.json({ message: 'Product deleted' })
+        await res.product.remove();
+        res.json({ message: 'Product deleted' });
     } catch (error) {
-        res.status(500).json({ message: error.message })
+        res.status(500).json({ message: error.message });
     }
 })
 
@@ -48,13 +62,13 @@ async function getProducts(req, res, next) {
     try {
         product = await Products.findById(req.params.id);
         if (product === null) {
-            return res.status(404).json({ message: 'Can not find product' })
+            return res.status(404).json({ message: 'Can not find product' });
         }
     } catch (error) {
-        return res.status(500).json({ message: error.message })
+        return res.status(500).json({ message: error.message });
     }
 
-    res.product = product
+    res.product = product;
     next()
 }
 
