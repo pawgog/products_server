d86a150c1a8a545eed8f512041c22d71240fe6a3
pawgog <pgogola10@gmail.com>
2023-01-27 17:26:03 +0100

    Change port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3210;
 
 require('dotenv').config();
 
