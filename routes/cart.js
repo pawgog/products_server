6fe0b27a7ce8ed3047df660540815c3254375896
pawgog <pgogola10@gmail.com>
2023-02-23 22:10:19 +0100

    Simplified cart post request

--- routes/cart.js
+++ routes/cart.js
@@ -2,6 +2,22 @@ const express = require('express');
 const router = express.Router();
 const Cart = require('../models/cart');
 
+const cartArray = (productId, title, prices, url, imageUrl) => ({
+    productId,
+    title,
+    prices,
+    url,
+    imageUrl,
+    quantity: 1,
+});
+
+const calculatePrices = (cart, productId) => {
+    const { prices } = cart.cart.find((product) => (product.productId === productId))
+    return cart.pricesSum.map((product) => prices.find((price) => { 
+        if(price.currency === product.currency) return product.amount = product.amount + price.amount 
+    }))
+}
+
 router.get('/', async (req, res) => {
     try {
         const productsCart = await Cart.find();
@@ -16,18 +32,16 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/:id', async (req, res) => {
+    const { productId, prices, title, url, imageUrl } = req.body;
     try {
         const existCart = await Cart.findOne();
-        let cart = await Cart.findOne({ "cart.productId": req.body.productId });
+        let cart = await Cart.findOne({ "cart.productId": productId });
 
         if (existCart) {
             if (cart) {
-                const { prices } = cart.cart.find((product) => (product.productId === req.body.productId))
-                cart.pricesSum.map((product) => prices.find((price) => { 
-                    if(price.currency === product.currency) return product.amount = product.amount + price.amount 
-                }))
+                calculatePrices(cart, productId);
                 cart.cart.map((product) => {
-                    if(product.productId === req.body.productId) return product.quantity = product.quantity + 1;
+                    if(product.productId === productId) return product.quantity = product.quantity + 1;
                     return product;
                 });
 
@@ -35,18 +49,8 @@ router.post('/:id', async (req, res) => {
                 cart = await cart.save();
                 return res.status(201).send(cart);
             } else {
-                existCart.pricesSum.map((product) => req.body.prices.find((price) => { 
-                    if(price.currency === product.currency) return product.amount = product.amount + price.amount 
-                }))
-                const cartArray = {
-                    productId: req.body.productId,
-                    title: req.body.title,
-                    prices: req.body.prices,
-                    url: req.body.url,
-                    imageUrl: req.body.imageUrl,
-                    quantity: 1,
-                }
-                existCart.cart.push(cartArray);
+                existCart.cart.push(cartArray(productId, title, prices, url, imageUrl));
+                calculatePrices(existCart, productId);
 
                 existCart.markModified('pricesSum');
                 const updateProductCart = await existCart.save();
@@ -54,15 +58,8 @@ router.post('/:id', async (req, res) => {
             }
         } else {
             const productCart = new Cart({
-                cart: [{
-                    productId: req.body.productId,
-                    title: req.body.title,
-                    prices: req.body.prices,
-                    url: req.body.url,
-                    imageUrl: req.body.imageUrl,
-                    quantity: 1,
-                }],
-                pricesSum: req.body.prices
+                cart: [cartArray(productId, title, prices, url, imageUrl)],
+                pricesSum: prices
             });
 
             const newProductCart = await productCart.save();
